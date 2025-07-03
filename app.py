c7b72f9c1972cfc831ba764833e793a3f493b876
Wilber E. Cadena Bautista <110375373+W-Cadena@users.noreply.github.com>
2025-07-02 18:26:41 -0600

    Update app.py

--- app.py
+++ app.py
@@ -83,5 +83,7 @@ def obtener_datos():
 def servir_imagen(nombre):
     return send_from_directory(UPLOAD_FOLDER, nombre)
 
+port = int(os.environ.get("PORT", 5000))
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=port)
